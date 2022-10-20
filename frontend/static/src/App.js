0412123ad84d00418c41c7a8d97a1323eedf4e3e
Alec Hansen <alecdhansen@gmail.com>
2022-10-20 12:28:31 -0400

    add logout

--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -10,23 +10,32 @@ function App() {
   const [state, setState] = useState("");
   const [auth, setAuth] = useState(!!Cookies.get("Authorization"));
 
-  // async function logOut() {
-  //   const response = await fetch("/rest-auth/logout/", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       // 'X-CSRFToken': Cookies.get('csrftoken'),
-  //     },
-  //     body: JSON.stringify(),
-  //   });
-  //   if (!response.ok) {
-  //     console.warn(response);
-  //   } else {
-  //     const data = await response.json();
-  //     Cookies.remove("Authorization");
-  //     setAuth(false);
-  //   }
-  // }
+  const handleError = (err) => {
+    console.warn(err);
+  };
+
+  const logout = async (e) => {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": Cookies.get("csrftoken"),
+      },
+      body: JSON.stringify(state),
+    };
+    const response = await fetch("/dj-rest-auth/logout/", options).catch(
+      handleError
+    );
+    if (!response.ok) {
+      throw new Error("Oops! Something went wrong");
+    } else {
+      const data = await response.json();
+      Cookies.remove("Authorization", `Token${" "}${data.key}`);
+      document.cookie =
+        "sessionid=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+      await setAuth(false);
+    }
+  };
 
   return (
     <div className="body">
@@ -35,9 +44,13 @@ function App() {
           <h2 className="title">CHATTN</h2>
           <span className="subtitle">never be lonely again</span>
         </div>
-        <Button className="button" onClick={() => setAuth(false)}>
-          Logout
-        </Button>
+        {auth ? (
+          <Button className="button" onClick={() => logout()}>
+            Logout
+          </Button>
+        ) : (
+          ""
+        )}
       </header>
       <div>
         {auth ? <ChatApp /> : <LoadPage setAuth={() => setAuth(true)} />}
